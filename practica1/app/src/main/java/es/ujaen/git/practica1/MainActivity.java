65438d953678c65ce350830c8e0a5653b2833b1a
EmilioSanchezCatalan <esc00019@red.ujaen.es>
2016-09-22 13:28:14 +0200

    haciendo pruebas en practicas

--- practica1/app/src/main/java/es/ujaen/git/practica1/MainActivity.java
+++ practica1/app/src/main/java/es/ujaen/git/practica1/MainActivity.java
@@ -2,12 +2,28 @@ package es.ujaen.git.practica1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        but = (Button) findViewById(R.id.main_button_hello);
+        but.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.main_button_hello:
+                Toast toast = Toast.makeText(this, R.string.hello, Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+        }
     }
 }
